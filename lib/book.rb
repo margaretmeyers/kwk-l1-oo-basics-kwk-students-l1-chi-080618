7b45a12c3f63731dc66859c7865de8ecc230947f
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-10 15:49:06 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -8,9 +8,9 @@ class Book
   def initialize(title)
     @title = title
   end
-  # # def title
-  # #   @title 
-  # # end
+  # def title
+  #   @title 
+  # end
   # def author=(author)
   #   @author = author
   # end 
